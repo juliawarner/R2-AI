9444f51de8fcfb43d199afa658da4966d01a2f86
Julia Warner <juliawarner@Julias-MacBook-Pro-4.local>
2020-01-17 15:59:15 -0500

    added new sounds

--- manual_control.py
+++ manual_control.py
@@ -1,6 +1,7 @@
 from gpiozero import Robot
 from gpiozero import RGBLED
 import pygame
+import random
 
 #R2-D2 class
 class R2_D2():
@@ -11,6 +12,13 @@ class R2_D2():
 		self.turning_left = False
 		self.moving_forward = False
 		self.moving_backward = False
+
+        #light variables
+        #used to blink red-blue light
+        self.light = RGBLED(red=16, green=20, blue=21)
+        self.light.color = (1, 0, 0)
+        self.red = True
+        self.time_since_last_blink = 0
 
 	#function calls Robot.right() if not already moving right
 	def turn_right(self):
@@ -61,39 +69,34 @@ class R2_D2():
 		if(not moving):
 			self.legs.stop()
 
-
-	#initializes R2's sounds
-	#call only after pygame.init has been called
-	def init_sounds(self):
-		self.scream_sound = pygame.mixer.Sound('./sounds/scream.wav')
-		self.helpme_long_sound = pygame.mixer.Sound('./sounds/helpme_long.wav')
-		self.helpme_short_sound = pygame.mixer.Sound('./sounds/helpme_short.wav')
-		self.chatter1_sound = pygame.mixer.Sound('./sounds/chatter1.wav')
-		self.chatter2_sound = pygame.mixer.Sound('./sounds/chatter2.wav')
-		self.chatter3_sound = pygame.mixer.Sound('./sounds/chatter3.wav')
-		self.chatter4_sound = pygame.mixer.Sound('./sounds/chatter4.wav')
-		self.chatter5_sound = pygame.mixer.Sound('./sounds/chatter5.wav')
-		self.chatter6_sound = pygame.mixer.Sound('./sounds/chatter6.wav')
-		self.chatter6_sound = pygame.mixer.Sound('./sounds/chatter7.wav')
-		self.cute_sound = pygame.mixer.Sound('./sounds/cute.wav')
-		self.excited_sound = pygame.mixer.Sound('./sounds/excited.wav')
-		self.insistent_sound = pygame.mixer.Sound('./sounds/insistent.wav')
-		self.laughter_sound = pygame.mixer.Sound('./sounds/laughter.wav')
-		self.no_sound = pygame.mixer.Sound('./sounds/no.wav')
-		self.processing_sound = pygame.mixer.Sound('./sounds/processing.wav')
-		self.reallysad_sound = pygame.mixer.Sound('./sounds/reallysad.wav')
-		self.sad_sound = pygame.mixer.Sound('./sounds/sad.wav')
-		self.searching_whistle_sound = pygame.mixer.Sound('./sounds/searching_whistle.wav')
-		self.unhappy_sound = pygame.mixer.Sound('./sounds/unhappy.wav')
-		self.warning_sound = pygame.mixer.Sound('./sounds/warning.wav')
-		self.yes_sound = pygame.mixer.Sound('./sounds/yes.wav')
-
 	#plays a sound based on given input, does nothing if input invalid
 	#assumes pygame is initialized 
 	def play_sound(self, sound_name):
 		sound_location = './sounds/' + sound_name + '.wav'
 		sound = pygame.mixer.Sound(sound_location)
 		pygame.mixer.Sound.play(sound)
+
+    #updates all time dependent component of R2-D2
+    #expected delta_time in seconds
+    def update(self, delta_time):
+        #update light
+        self.update_light(delta_time)
+
+    #keeps track of time since last light blink and blinks light
+    #if that time is greater than 1 second. 
+    #delta time parameter is in seconds
+    def update_light(self, delta_time):
+        self.time_since_last_blink = self.time_since_last_blink + delta_time
+
+        #blink about every 1 second
+        if(self.time_since_last_blink >= 1.0):
+            self.time_since_last_blink = 0
+            if(self.red):
+                self.red = False
+                self.light.color = (0, 0, 1)
+            else:
+                self.red = True
+                self.light.color = (1, 0, 0)
 
 #create an artoo
 artoo = R2_D2()
@@ -124,8 +127,17 @@ while(running):
 			if event.key == pygame.K_s:
 				artoo.play_sound('scream')
 			if event.key == pygame.K_e:
-				artoo.play_sound('excited')
-			#quit by pressing q key
+				artoo.play_sound('yes')
+            if event.key == pygame.K_c:
+                randomChatter = random.randint(1,8)
+                chatterName = 'chatter' + str(randomChatter)
+                artoo.play_sound(chatterName)
+            if event.key == pygame.K_l:
+                artoo.play_sound('helpme_long')
+            if event.key == pygame.K_u:
+                artoo.play_sound('unhappy')
+
+ 			#quit by pressing q key
 			if event.key == pygame.K_q:
 				running = False
 
